6a245ea8230e8f2fe1aebfb8373c9f7e0d452d1e
Oz Ramos <pixelfelt@gmail.com>
2021-04-01 17:36:21 -0700

    #56 Sets up vue monaco and pug, but not working yet

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,100 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="pug">
+MonacoEditor(ref='editor' v-model='code' language='javascript' :options='settings' @editorDidMount='editorDidMount' theme='shadesofpurple')
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MonacoEditor from 'vue-monaco'
 
 export default {
   name: 'App',
+  
   components: {
-    HelloWorld
+    MonacoEditor
+  },
+
+  data () {
+    return {
+      code: '',
+      
+      settings: {
+        theme: 'shadesofpurple',
+        automaticLayout: true,
+        minimap: {enabled: false}
+      }
+    }
+  },
+
+  methods: {
+    /**
+     * Setup the editor
+     * - Theme
+     * - Set options
+     * - Autoheight if extraOptions.autoheight
+     */
+    editorDidMount () {
+      const monaco = this.$refs.editor.monaco
+      this.$refs.editor.getMonaco().getModel().updateOptions({tabSize: 2})
+  
+      monaco.editor.defineTheme('shadesofpurple', {
+        base: 'vs-dark',
+        inherit: true,
+        rules: [
+          { token: '', foreground: '9EFFFF', background: '9EFFFF' },
+          // { token: 'invalid', foreground: 'ff0000' },
+          { token: 'emphasis', fontStyle: 'italic' },
+          { token: 'strong', fontStyle: 'bold' },
+
+          { token: 'variable', foreground: 'ff0000' },
+          // { token: 'variable.predefined', foreground: 'ff0000' },
+          { token: 'variable.parameter', foreground: '9EFFFF' },
+          // { token: 'constant', foreground: 'ff0000' },
+          { token: 'comment', foreground: 'B362FF' },
+          { token: 'number', foreground: 'FF628C' },
+          { token: 'number.hex', foreground: 'FF628C' },
+          { token: 'regexp', foreground: 'FB94FF' },
+          // { token: 'annotation', foreground: 'ff0000' },
+          { token: 'type', foreground: '9EFFFF' },
+
+          { token: 'delimiter', foreground: 'ffffff' },
+
+          { token: 'attribute.name', foreground: 'ff0000' },
+          { token: 'attribute.value', foreground: 'ff0000' },
+
+          { token: 'string', foreground: 'A5FF90' },
+
+          { token: 'keyword', foreground: 'FF9D00' }
+        ],
+
+        // @see https://code.visualstudio.com/api/references/theme-color
+        colors: {
+          'editorIndentGuide.background': '#A599E90F',
+          'editorIndentGuide.activeBackground': '#A599E942',
+          'editor.background': '#2d2b55',
+          'editor.foreground': '#ffffff',
+          'editor.selectionBackground': '#7448af',
+          'editor.lineHighlightBackground': '#1f1f41',
+          'editorCursor.foreground': '#fad000',
+          'editorWhitespace.foreground': 'red'
+        }
+      })
+      monaco.editor.setTheme('shadesofpurple')
+
+      // Autoheight
+      if (this.$props.extraOptions.autoheight) {
+        this.updateHeight()
+        this.$refs.editor.editor.onDidContentSizeChange(this.updateHeight)
+      }
+    },
+
+    /**
+     * Autoresize
+     */
+    updateHeight () {
+      const contentHeight = Math.min(1000, this.$refs.editor.editor.getContentHeight()) + 20
+
+      this.$refs.editor.$el.style.height = `${contentHeight}px`
+      this.$refs.editor.editor.layout({ height: contentHeight })
+    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,17 @@ const pkg = require('./package.json')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? `/pixelfelt-sites@${pkg.version}/dist/` : '/',
-  filenameHashing: false
+  filenameHashing: false,
+
+  configureWebpack: config => {
+    const MonacoEditorPlugin = require('monaco-editor-webpack-plugin')
+      config.plugins.push(new MonacoEditorPlugin({
+        // https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+        // Include a subset of languages support
+        // Some language extensions like typescript are so huge that may impact build performance
+        // e.g. Build full languages support with webpack 4.0 takes over 80 seconds
+        // Languages are loaded on demand at runtime
+        languages: ['javascript', 'typescript', 'css', 'html', 'json']
+      }))
+  }
 }
